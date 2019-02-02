927699010fe62c188bc49795e032c9d1a4980305
alex <alex@rocketguys.com>
2019-02-03 00:44:43 +0300

    add LAST4, Beep, DEBUG

--- pan-scanner.ino
+++ pan-scanner.ino
@@ -1,3 +1,10 @@
+// #define DEBUG
+
+#ifdef DEBUG
+#define MIFAREDEBUG
+#define PN532DEBUG
+#endif
+
 #include <SPI.h>
 #include <Adafruit_PN532.h>
 
@@ -6,77 +13,138 @@
 USBHID HID;
 HIDKeyboard Keyboard(HID);
 
-#define PN532_SCK  (2)
+#define LAST4
+
+#define PN532_SCK (2)
 #define PN532_MOSI (3)
-#define PN532_SS   (4)
+#define PN532_SS (4)
 #define PN532_MISO (5)
 
 Adafruit_PN532 nfc(PN532_SS);
 
-void PrintPAN(const byte * data, const uint32_t numBytes)
+void PrintPAN(const byte *data)
 {
-  uint32_t szPos;
-  for (szPos=5; szPos < 13; szPos++)
+  uint32_t szPos = 5;
+#ifdef LAST4
+  szPos += (16 - 4) / 2;
+#endif
+  for (; szPos < 13; szPos++) // 8 итераций по 2 PAN-цифры в каждом числе за раз
   {
     // Append leading 0 for small values
-    if (data[szPos] <= 0xF) Keyboard.print(F("0"));
+    if (data[szPos] <= 0xF)
+      Keyboard.print(F("0"));
     Keyboard.print(data[szPos], HEX);
   }
 }
 
-void setup(void) {
-  HID.begin(HID_KEYBOARD);
-  Keyboard.begin(); // useful to detect host capslock state and LEDs
-  
-  Serial.begin(115200);
-  Serial.println(".");
-  
-  delay(1000);
- 
-  
-  nfc.begin();
-
-  uint32_t versiondata = nfc.getFirmwareVersion();
-  if (! versiondata) {
-    Keyboard.println("Didn't find PN53x board");
-    while (1); // halt
-  }
-  
-  Keyboard.println("Found chip");
-  nfc.setPassiveActivationRetries(0xFF);
-  nfc.SAMConfig();
-  Keyboard.println("Waiting for an ISO14443A card");
+// medium duration, single impulse
+void Beep()
+{
+  digitalWrite(PB1, HIGH);
+  delay(100);
+  digitalWrite(PB1, LOW);
 }
 
+// short-short
+void BeepReady()
+{
+  digitalWrite(PB1, HIGH), delay(100), digitalWrite(PB1, LOW);
+  delay(100);
+  digitalWrite(PB1, HIGH), delay(100), digitalWrite(PB1, LOW);
+}
 
+// long-short-long
+void BeepError()
+{
+  digitalWrite(PB1, HIGH), delay(500), digitalWrite(PB1, LOW);
+  delay(100);
+  digitalWrite(PB1, HIGH), delay(100), digitalWrite(PB1, LOW);
+  delay(100);
+  digitalWrite(PB1, HIGH), delay(1000), digitalWrite(PB1, LOW);
+}
 
-void loop(void) {
-  bool success; 
-  uint8_t SELECT_APPLICATION[]    = {0x00, 0xA4, 0x04, 0x00, 0x07, 0xA0, 0x00, 0x00, 0x00, 0x04, 0x10, 0x10, 0x00};  
-  uint8_t GET_PROCESSING_OPTIONS[]    = {0x80, 0xA8, 0x00, 0x00, 0x02, 0x83, 0x00, 0x00};  
-  uint8_t READ_RECORD[]    = {0x00, 0xB2, 0x01, 0x14, 0x00};  
+void setup(void)
+{
+  HID.begin(HID_KEYBOARD);
+  Keyboard.begin(); // useful to detect host capslock state and LEDs
+  //Serial.begin(115200);
+  pinMode(PB1, OUTPUT); // BEEP_PIN
+  //delay(1000);
+  nfc.begin();
+
+#ifdef DEBUG
+  Keyboard.println("nfc, keyboard, serial began, pins initialized");
+#endif
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (!versiondata)
+  {
+#ifdef DEBUG
+    Keyboard.println("Didn't find PN53x board");
+#endif
+    BeepError();
+    while (1)
+      ; // halt
+  }
+
+#ifdef DEBUG
+  Keyboard.println("Found chip");
+#endif
+
+  // Set the max number of retry attempts to read from a card
+  // This prevents us from waiting forever for a card, which is
+  // the default behaviour of the PN532.
+  // 0xFF to wait forever, 0x00..0xFE to timeout after mxRetries
+  nfc.setPassiveActivationRetries(0xFF);
+
+  nfc.SAMConfig();
+  // Keyboard.println("Waiting for an ISO14443A card");
+
+#ifdef DEBUG
+  Keyboard.println("ready");
+#endif
+
+  BeepReady();
+}
+
+void loop(void)
+{
+  bool success;
+  uint8_t SELECT_APPLICATION[] = {0x00, 0xA4, 0x04, 0x00, 0x07, 0xA0, 0x00, 0x00, 0x00, 0x04, 0x10, 0x10, 0x00};
+  uint8_t GET_PROCESSING_OPTIONS[] = {0x80, 0xA8, 0x00, 0x00, 0x02, 0x83, 0x00, 0x00};
+  uint8_t READ_RECORD[] = {0x00, 0xB2, 0x01, 0x14, 0x00};
 
   uint8_t response[64];
   uint8_t responseLength = sizeof(response);
-  Keyboard.println("Hello!");
+#ifdef DEBUG
+  Keyboard.println(".");
+#endif
   success = nfc.inListPassiveTarget();
 
-  if(success) {
-          Keyboard.println("Found something!");
-          if (nfc.inDataExchange(SELECT_APPLICATION, sizeof(SELECT_APPLICATION), response, &responseLength)) {
-              nfc.PrintHexChar(response, responseLength);
-          }
-          if (nfc.inDataExchange(GET_PROCESSING_OPTIONS, sizeof(GET_PROCESSING_OPTIONS), response, &responseLength)) {
-              nfc.PrintHexChar(response, responseLength);
-          }
-          if (nfc.inDataExchange(READ_RECORD, sizeof(READ_RECORD), response, &responseLength)) {
-              nfc.PrintHexChar(response, responseLength);
-              Keyboard.println("Yep that's a PAN");
-              PrintPAN(response, responseLength);
-          }
-              delay(1000);
-          } else {
-              Keyboard.println("Timed out waiting for a card");
-          }
+  if (success)
+  {
+#ifdef DEBUG
+    Keyboard.println("Found something!");
+#endif
+    if (nfc.inDataExchange(SELECT_APPLICATION, sizeof(SELECT_APPLICATION), response, &responseLength))
+    {
+      //nfc.PrintHexChar(response, responseLength);
+    }
+    if (nfc.inDataExchange(GET_PROCESSING_OPTIONS, sizeof(GET_PROCESSING_OPTIONS), response, &responseLength))
+    {
+      //nfc.PrintHexChar(response, responseLength);
+    }
+    if (nfc.inDataExchange(READ_RECORD, sizeof(READ_RECORD), response, &responseLength))
+    {
+      //nfc.PrintHexChar(response, responseLength); // Ничего не печатает
+      PrintPAN(response);
+      Keyboard.println("");
+      Beep();
+    }
+  }
 
+  else
+  {
+    //              Keyboard.println("Timed out waiting for a card");
+  }
 }
